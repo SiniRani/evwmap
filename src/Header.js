5f85315f964856362b31a99ea3469f346c36dc12
SiniRani <kul0107@g.yju.ac.kr>
2021-04-15 19:42:47 +0900

    Header

--- src/Header.js
+++ src/Header.js
@@ -5,144 +5,272 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Heam from "./Heam.js";
+import $ from "jquery";
 
 function Header(props) {
-  return (
-    <>
-      <div className="dHead">
-        <div className="header-wrap">
-          <div className="inner">
-            <h2 className="headerh2">
-              <a href="#" className="arrow">
-                <FontAwesomeIcon
-                  icon={faBars}
-                  className="arrow-btn"
-                  onClick={() => {
-                    props.setMenu(!props.menu);
-                  }}
-                />
-              </a>
-            </h2>
-            <h1 className="mainlogo">
-              <Link to="/main">
-                <a href="#" className="Logo">
-                  "EV WMap"
-                </a>
-              </Link>
-            </h1>
-            <Heam />
-          </div>
-        </div>
-      </div>
-      <div className="null">{props.menu === true ? <Heam2 /> : null}</div>
-    </>
-  );
+    // $(function () {
+    //     $(".header-back").hover(
+    //         function () {
+    //             $(".main_bar").css("color", "black");
+    //         },
+    //         function () {
+    //             $(".main_bar").css("color", "white");
+    //         }
+    //     );
+    // });
+    // $(function () {
+    //   $(document).ready(function () {
+    //   })
+    // })
+    $(function () {
+        var headerScroll = $(".header-back").offset();
+        $(window).on("beforeunload", function () {
+            $(window).scrollTop(0);
+        });
+        $(document).ready(function () {
+            $(".inner").mouseover(function () {
+                $(".sub-wrap").css("opacity", "1");
+                $(".sub-wrap").css("transform", "translateY(0px)");
+                $(".main_bar").css("color", "black");
+                $(".login_box").css("color", "black");
+                $(".header-back").css("opacity", "1");
+                $(".header-back").css("transform", "translateY(0px)");
+                $(".header-back").css("border-bottom", "none");
+                // if ($(document).scrollTop() > headerScroll.top) {
+                // }
+            });
+            $(".sub-wrap").mouseleave(function () {
+                $(".sub-wrap").css("opacity", "0");
+                $(".sub-wrap").css("transform", "translateY(-90px)");
+
+                if ($(document).scrollTop() > headerScroll.top) {
+                    $(".header-back").css("opacity", "1");
+                    $(".main_bar").css("color", "black");
+                    $(".login_box").css("color", "black");
+                    $(".header-back").css("border-bottom", "1px solid #d4d4d4");
+                } else {
+                    $(".header-back").css("opacity", "0");
+                    $(".header-back").css("transform", "translateY(0px)");
+                    $(".main_bar").css("color", "white");
+                    $(".login_box").css("color", "white");
+                }
+            });
+            $(window).scroll(function () {
+                var scroll = $(window).scrollTop();
+
+                console.log("백그라운드" + headerScroll.top);
+                console.log(scroll);
+                console.log("헤더 스크롤" + 0);
+                if ($(document).scrollTop() > headerScroll.top) {
+                    $(".header-back").css("opacity", "1");
+                    $(".header-back").css("border-bottom", "1px solid #d4d4d4");
+                    $(".main_bar").css("color", "black");
+                    $(".login_box").css("color", "black");
+                } else {
+                    $(".header-back").css("opacity", "0");
+                    $(".main_bar").css("color", "white");
+                    $(".login_box").css("color", "white");
+                }
+            });
+        });
+    });
+
+    return (
+        <>
+            <div className="dHead">
+                <div className="header-back" />
+                <div className="header-wrap">
+                    <div className="inner">
+                        <h2 className="headerh2">
+                            <a href="#" className="arrow">
+                                <FontAwesomeIcon
+                                    icon={faBars}
+                                    className="arrow-btn"
+                                    onClick={() => {
+                                        props.setMenu(!props.menu);
+                                    }}
+                                />
+                            </a>
+                        </h2>
+                        <h1 className="mainlogo">
+                            <Link to="/">
+                                <a href="#" className="Logo">
+                                    "EV WMap"
+                                </a>
+                            </Link>
+                        </h1>
+                        <Heam />
+                    </div>
+                </div>
+                <div className="sub-wrap on">
+                    <div className="sub sub_left">
+                        <div className="sub-list">
+                            <ul className="sub-list_ul sub-ul-01">
+                                <li className="sub-list_li">
+                                    <Link to="/map">
+                                        <a href="">충전소 조회</a>
+                                    </Link>
+                                </li>
+                                <li className="sub-list_li">
+                                    <Link to="/navigate">
+                                        <a href="">길찾기</a>
+                                    </Link>
+                                </li>
+                            </ul>
+                            <ul className="sub-list_ul sub-ul-02">
+                                <li className="sub-list_li">
+                                    <Link to="/notice">
+                                        <a href="">공지사항</a>
+                                    </Link>
+                                </li>
+
+                                <li className="sub-list_li">
+                                    <Link to="/freeboard">
+                                        <a href="">자유게시판</a>
+                                    </Link>
+                                </li>
+                                <li className="sub-list_li">
+                                    <Link to="/tipboard">
+                                        <a href="">TIP게시판</a>
+                                    </Link>
+                                </li>
+                                <li className="sub-list_li">
+                                    <Link to="/news">
+                                        <a href="">뉴스게시판</a>
+                                    </Link>
+                                </li>
+                            </ul>
+                            <ul className="sub-list_ul sub-ul-03">
+                                <li className="sub-list_li">
+                                    <Link to="/faq">
+                                        <a href="">FAQ</a>
+                                    </Link>
+                                </li>
+                                <li className="sub-list_li">
+                                    <Link to="/questions">
+                                        <a href="">문의하기</a>
+                                    </Link>
+                                </li>
+                                <li className="sub-list_li">
+                                    <Link to="/qlist">
+                                        <a href="">문의내역</a>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="null">{props.menu === true ? <Heam2 /> : null}</div>
+        </>
+    );
 }
 
 function Heam2() {
-  return (
-    <>
-      <div className="end"></div>
-      <div className="MH">
-        <div className="heammenu">
-          <div className="joinbox">
-            <ul className="global_join_box">
-              <li className="global_list">
-                <a className="login_box" href="#">
-                  로그인
-                </a>
-              </li>
-              <li className="global_list">
-                <a className="join_box" href="#">
-                  회원가입
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="global_menu">
-            <nav className="gub-wrap">
-              <ul className="mainmenu_ul">
-                <li className="area01 main_li">
-                  <p className="out">
-                    <p className="in" href="#">
-                      소개
-                    </p>
-                  </p>
-                </li>
-
-                <li className="area02 main_li">
-                  <p className="out">
-                    <p className="in">지도</p>
-                  </p>
-                  <div className="sub">
-                    <div className="sub-list">
-                      <ul className="sub-list_ul">
-                        <li className="sub-list_li">
-                          <a href="">길찾기</a>
-                        </li>
-                        <li className="sub-list_li">
-                          <a href="">예약</a>
-                        </li>
-                        <li className="sub-list_li">
-                          <a href="">조회</a>
-                        </li>
-                      </ul>
+    return (
+        <>
+            <div className="end"></div>
+            <div className="MH">
+                <div className="heammenu">
+                    <div className="joinbox">
+                        <ul className="global_join_box">
+                            <li className="global_list">
+                                <a className="login_box" href="#">
+                                    로그인크크
+                                </a>
+                            </li>
+                            <li className="global_list">
+                                <a className="join_box" href="#">
+                                    회원가입
+                                </a>
+                            </li>
+                        </ul>
                     </div>
-                  </div>
-                </li>
+                    <div className="global_menu">
+                        <nav className="gub-wrap">
+                            <ul className="mainmenu_ul">
+                                <li className="area01 main_li">
+                                    <p className="out">
+                                        <p className="in" href="#">
+                                            소개
+                                        </p>
+                                    </p>
+                                </li>
 
-                <li className="area03 main_li">
-                  <p className="out">
-                    <p className="in">커뮤니티</p>
-                  </p>
-                  <div className="sub">
-                    <div className="sub-list">
-                      <ul className="sub-list_ul">
-                        <Link to="/notice">
-                          <li className="sub-list_li">
-                            <a href="">공지사항</a>
-                          </li>
-                        </Link>
-                        <Link to="/freeboard">
-                          <li className="sub-list_li">
-                            <a href="">게시판</a>
-                          </li>
-                        </Link>
-                        <li className="sub-list_li">
-                          <a href="">뉴스</a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </li>
+                                <li className="area02 main_li">
+                                    <p className="out">
+                                        <p className="in">지도</p>
+                                    </p>
+                                    <div className="sub">
+                                        <div className="sub-list">
+                                            <ul className="sub-list_ul">
+                                                <li className="sub-list_li">
+                                                    <a href="">길찾기</a>
+                                                </li>
+                                                <li className="sub-list_li">
+                                                    <a href="">예약</a>
+                                                </li>
+                                                <li className="sub-list_li">
+                                                    <a href="">조회</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </li>
 
-                <li className="area04 main_li">
-                  <p className="out">
-                    <p className="in">문의하기</p>
-                  </p>
-                  <div className="sub">
-                    <div className="sub-list">
-                      <ul className="sub-list_ul">
-                        <li className="sub-list_li">
-                          <a href="">FAQ</a>
-                        </li>
-                        <li className="sub-list_li">
-                          <a href="">문의하기</a>
-                        </li>
-                        <li className="sub-list_li">
-                          <a href="">문의내역</a>
-                        </li>
-                      </ul>
+                                <li className="area03 main_li">
+                                    <p className="out">
+                                        <p className="in">커뮤니티</p>
+                                    </p>
+                                    <div className="sub">
+                                        <div className="sub-list">
+                                            <ul className="sub-list_ul">
+                                                <Link to="/notice">
+                                                    <li className="sub-list_li">
+                                                        <a href="">공지사항</a>
+                                                    </li>
+                                                </Link>
+                                                <Link to="/freeboard">
+                                                    <li className="sub-list_li">
+                                                        <a href="">게시판</a>
+                                                    </li>
+                                                </Link>
+                                                <li className="sub-list_li">
+                                                    <a href="">뉴스</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </li>
+
+                                <li className="area04 main_li">
+                                    <p className="out">
+                                        <p className="in">문의하기</p>
+                                    </p>
+                                    <div className="sub">
+                                        <div className="sub-list">
+                                            <ul className="sub-list_ul">
+                                                <li className="sub-list_li">
+                                                    <a href="">FAQ</a>
+                                                </li>
+                                                <li className="sub-list_li">
+                                                    <a href="">문의하긔기</a>
+                                                </li>
+                                                <li className="sub-list_li">
+                                                    <a href="">문의내크역</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </nav>
                     </div>
-                  </div>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Header;
--- src/Notice.js
+++ src/Notice.js
@@ -1,218 +1,127 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./css/Notice.scss";
-import "./css/NoticeBoard.scss";
 import APagination from "@material-ui/lab/Pagination";
 import { Link } from "react-router-dom";
 import BannerNotice from "./BannerNotice";
 
 function Notice() {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(10);
-
-  useEffect(async () => {
-    var data = JSON.stringify({
-      page: 1,
-      keyWord: "",
-      ser: "",
-      cat_cd: "0",
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage, setPostsPerPage] = useState(10);
+    const data = JSON.stringify({
+        u_id: "youngsik1",
     });
 
-    var config = {
-      method: "get",
-      url: "http://3.36.160.255:8081/api/board/list",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: data,
-    };
-
-    axios(config)
-      .then(function (response) {
-        console.log(JSON.stringify(response.data));
+    useEffect(async () => {
+        setLoading(true);
+        const response = await axios(
+            "https://jsonplaceholder.typicode.com/posts"
+        );
         setPosts(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }, []);
+        setLoading(false);
+        console.log(posts);
+    }, []);
 
-  const indexOfLast = currentPage * postsPerPage;
-  const indexOfFirst = indexOfLast - postsPerPage;
-  function currentPosts(tmp) {
-    let currentPosts = 0;
-    currentPosts = tmp.slice(indexOfFirst, indexOfLast);
-    return currentPosts;
-  }
-  return (
-    <div className="comunityTop">
-      <Posts posts={currentPosts(posts)} loading={loading} />
-      <Pagination
-        postsPerPage={postsPerPage}
-        totalPosts={posts.length}
-        paginate={setCurrentPage}
-        currentPage={currentPage}
-        page={currentPage}
-        setCurrentPage={setCurrentPage}
-        setPostsPerPage={setPostsPerPage}
-      ></Pagination>
-    </div>
-  );
+    const indexOfLast = currentPage * postsPerPage;
+    const indexOfFirst = indexOfLast - postsPerPage;
+    function currentPosts(tmp) {
+        let currentPosts = 0;
+        currentPosts = tmp.slice(indexOfFirst, indexOfLast);
+        return currentPosts;
+    }
+    return (
+        <div className="comunityTop">
+            <Posts posts={currentPosts(posts)} loading={loading} />
+            <Pagination
+                postsPerPage={postsPerPage}
+                totalPosts={posts.length}
+                paginate={setCurrentPage}
+                currentPage={currentPage}
+                page={currentPage}
+                setCurrentPage={setCurrentPage}
+                setPostsPerPage={setPostsPerPage}
+            ></Pagination>
+        </div>
+    );
 }
 function Posts({ posts, loading }) {
-  return (
-    <>
-      {/* <div className="end"></div> */}
-      <div data-aos="fade-down"
-        data-aos-duration="1000">
-        <BannerNotice />
-
-      <div className="NTlocationData">
-        <div className="inner">
-          <div className="btnHome">
-            <i class="fas fa-home"></i>
-          </div>
-          <div className="navTitle">
-            <ul className="ulTitle">
-              <li className="liTitleOpen">
-                <div className="navMenu">
-                  COMMUNITY
-                  <div className="navInnerMenu">
-                    <i class="fas fa-caret-down"></i>
-                  </div>
+    return (
+        <>
+            {/* <div className="end"></div> */}
+            <div data-aos="fade-down" data-aos-duration="1000">
+                <BannerNotice />
+            </div>
+            <div className="contentsNotice">
+                {/* <div className="start"></div> */}
+                <div className="banner">
+                    <p className="banner-title">공지사항</p>
+                    <br></br>
+                    <p className="subtitle">
+                        EV WMAP의 공지 및 업데이트 소식을 전합니다.
+                    </p>
                 </div>
-                <ul className="navList">
-                  <Link to="/introduction">
-                    <li>
-                      <a>INTRODUCTION</a>
-                    </li>
-                  </Link>
-                  <Link to="/map">
-                    <li>
-                      <a>MAP</a>
-                    </li>
-                  </Link>
-                  <Link to="/notice">
-                    <li>
-                      <a>COMMUNITY</a>
-                    </li>
-                  </Link>
-                  <Link to="/faq">
-                    <li>
-                      <a>CONTACT</a>
-                    </li>
-                  </Link>
-                </ul>
-              </li>
-            </ul>
-          </div>
-          <div className="navTitle">
-            <ul className="ulTitle">
-              <li className="liTitleOpen">
-                <a>
-                  <div className="navMenu">
-                    NOTICE
-                    <div className="navInnersMenu">
-                      <i class="fas fa-caret-down"></i>
-                    </div>
-                  </div>
-                </a>
-                <ul className="navList">
-                  <Link to="/notice">
-                    <li>
-                      <a>NOTICE</a>
-                    </li>
-                  </Link>
-                  <Link to="/freeboard">
-                    <li>
-                      <a>FREE BOARD</a>
-                    </li>
-                  </Link>
-                  <Link to="/tipboard">
-                    <li>
-                      <a>TIP BOARD</a>
-                    </li>
-                  </Link>
-                  <Link to="/news">
-                    <li>
-                      <a>NEWS BOARD</a>
-                    </li>
-                  </Link>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div className="contentsNotice">
-        {/* <div className="start"></div> */}
-        <div className="banner">
-          <p className="banner-title">공지사항</p>
-          <br></br>
-          <p className="subtitle">
-            EV WMAP의 공지 및 업데이트 소식을 전합니다.
-          </p>
-        </div>
-        <table className="list">
-          <thead>
-            <tr>
-              <th>번호</th>
-              <th className="th-title">제목</th>
-              <th>글쓴이</th>
-              <th className="th-date">작성일</th>
-              <th>조회</th>
-            </tr>
-          </thead>
-          <tbody>
-            {posts.map((post) => (
-              <tr>
-                <td>{post.cat_cd}</td>
-                <td key={post.cat_cd} className="td-title">
-                  <Link to={`/notice/${post.cat_cd}`}>{post.b_title}</Link>
-                </td>
-                <td>{post.u_id}</td>
-                <td>{post.b_dtt}</td>
-                <td>{post.b_visite}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </>
-  );
+                <table className="list">
+                    <thead>
+                        <tr>
+                            <th>번호</th>
+                            <th className="th-title">제목</th>
+                            <th>글쓴이</th>
+                            <th className="th-date">작성일</th>
+                            <th>조회</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {posts.map((post) => (
+                            <tr>
+                                <td>{post.id}</td>
+                                <td key={post.id} className="td-title">
+                                    <Link to={`/notice/${post.id}`}>
+                                        {post.title}
+                                    </Link>
+                                </td>
+                                <td>작성자</td>
+                                <td>작성일</td>
+                                <td>10</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
+    );
 }
 
 function Pagination({
-  postsPerPage,
-  totalPosts,
-  paginate,
-  page,
-  setCurrentPage,
-  setPostsPerPage,
+    postsPerPage,
+    totalPosts,
+    paginate,
+    page,
+    setCurrentPage,
+    setPostsPerPage,
 }) {
-  let pageNumber = 0;
-  const pageNumbers = [];
-  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
-    pageNumbers.push(i);
-    pageNumber += 1;
-  }
+    let pageNumber = 0;
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+        pageNumbers.push(i);
+        pageNumber += 1;
+    }
 
-  return (
-    <div className="pageNation">
-      <div>
-        <APagination
-          count={pageNumbers.length}
-          size="large"
-          onChange={handleChange}
-        />
-      </div>
-    </div>
-  );
-  function handleChange(e, value) {
-    paginate(value);
-  }
+    return (
+        <div className="pageNation">
+            <div>
+                <APagination
+                    count={pageNumbers.length}
+                    size="large"
+                    onChange={handleChange}
+                />
+            </div>
+        </div>
+    );
+    function handleChange(e, value) {
+        paginate(value);
+    }
 }
 
 export default Notice;
